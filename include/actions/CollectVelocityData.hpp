00dd4e79852fb600ee4a8ad647828e7ffa278837
Robert Hilton <robert.a.hilton.jr@gmail.com>
2022-01-10 00:25:51 +0000

    Update

---
+++ include/actions/CollectVelocityData.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <vector>
+#include "physics/DriveCharacterization.hpp"
+#include "ck_utilities/CKTimer.hpp"
+#include "ros/ros.h"
+
+
+class CollectVelocityData
+{
+public:
+    CollectVelocityData(ros::NodeHandle* node, std::vector<ck::physics::VelocityDataPoint>& data, bool highGear, bool reverse, bool turn);
+    void start();
+    void update(double leftRPM, double rightRPM);
+    bool isFinished();
+    void done();
+
+private:
+    static constexpr double kMaxPower = 0.25;
+    static constexpr double kRampRate = 0.02;
+
+    std::vector<ck::physics::VelocityDataPoint> *mVelocityData;
+    bool mTurn;
+    bool mReverse;
+    bool mHighGear;
+
+    bool mFinished = false;
+    double mStartTime = 0.0;
+
+    ck::ElapsedTimer eTimer;
+    ros::NodeHandle* mNode;
+
+    //static ros::Publisher *robot_drive_pub = node->advertise<rio_control_node::Robot_Status>("RobotStatus", 1);
+};
---
+++ src/actions/CollectVelocityData.cpp
@@ -0,0 +1,45 @@
+#include "actions/CollectVelocityData.hpp"
+#include <cmath>
+
+CollectVelocityData::CollectVelocityData(ros::NodeHandle* node, std::vector<ck::physics::VelocityDataPoint>& data, bool highGear, bool reverse, bool turn)
+{
+    mNode = node;
+    mVelocityData = &data;
+    mHighGear = highGear;
+    mReverse = reverse;
+    mTurn = turn;
+}
+
+void CollectVelocityData::start()
+{
+    //TODO: Set High Gear
+    eTimer.start();
+}
+
+void CollectVelocityData::update(double leftRPM, double rightRPM)
+{
+    double percentPower = kRampRate * (eTimer.hasElapsed());
+    if (percentPower > kMaxPower) {
+        mFinished = true;
+        return;
+    }
+
+    //Set Drive power
+    //mDrive.setOpenLoop(new DriveSignal((mReverse ? -1.0 : 1.0) * percentPower, (mReverse ? -1.0 : 1.0) * (mTurn ? -1.0 : 1.0) * percentPower));
+
+    mVelocityData->push_back(ck::physics::VelocityDataPoint{
+        (std::abs(leftRPM) + std::abs(rightRPM)) * M_PI / 60.0, //convert velocity to radians per second 
+        percentPower * 12.0 //convert to volts
+    });
+}
+
+bool CollectVelocityData::isFinished()
+{
+    return mFinished;
+}
+
+void CollectVelocityData::done()
+{
+    //Turn off Drive
+    //Flush data
+}
